4406b170eeb0beb7f688aca30f4ac9bb238b7ff1
Hayder <hayder.alhatemi@gmail.com>
2026-03-26 20:44:46 +0200

    fix: fix: rename MONGO_URL to MONGO_URI

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,5 +10,5 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-const MONGO_URL = process.env.MONGO_URL || '';
+const MONGO_URI = process.env.MONGO_URI || '';
 
