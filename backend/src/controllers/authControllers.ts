65a3d54247c7c590f74f8a4233981f403ad2d119
Hayder <hayder.alhatemi@gmail.com>
2026-03-27 14:35:39 +0200

    feat: implement user registeration with password hashing

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -3,3 +3,14 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User';
 
+export const register = async (req: Request, res: Response) => {
+    const { username, email, password } = req.body;
+    try {
+        const hashed = await bcrypt.hash(password, 10);
+        const user = await User.create({ username, email, password: hashed });
+        res.status(201).json({ message: 'User created', userId: user._id  });
+    } catch (err) {
+        res.status(400).json({ error: 'User already exists or invalid data' });
+    }
+};
+
