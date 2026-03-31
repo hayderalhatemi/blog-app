7c50c07c526b8a7732f9ed24bb240991086430f0
Hayder <hayder.alhatemi@gmail.com>
2026-03-31 13:12:31 +0300

    feat: register post routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/authRoutes';
+import postRoutes from './routes/postRoutes';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || '';
