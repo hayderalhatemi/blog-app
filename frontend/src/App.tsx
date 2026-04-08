a76b3f1b9392d2e72c8f06c640d65bb25624140b
Hayder <hayder.alhatemi@gmail.com>
2026-04-08 22:29:57 +0300

    feat: add routing for login and register pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<h1>Blog App</h1>} />
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Blog App</h1>} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
