87cabee7cbd322c0956ebd9e345f2e084b1053fa
Hayder <hayder.alhatemi@gmail.com>
2026-04-28 16:51:20 +0300

    fix: cleanup code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import Posts from './pages/Posts';
@@ -9,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
+      <Route path="/home" element={<Navigate to="/" replace />} />
       <Route path="/" element={<div className="page"><h1>Blog App</h1></div>} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
